2f27563c3a845a276d6e05ca5ed2b3bdb0ef9a7a
KevinCruzBasantes <kicruz@uce.edu.ec>
2025-11-07 19:44:37 -0500

    Taller 8 Maquetacion Pagina Web

---
+++ js/calculadorafinal.js
@@ -0,0 +1,23 @@
+//<!--variable local-->
+let num1=0;
+let num2=0;
+
+function mostrardisplay(valor){
+   let elemento= document.getElementById("display");
+    elemento.innerText =elemento.innerText+ valor;
+}
+function sumar(num1,num2){
+    return num1+num2;
+}
+function restar(num1,num2   ){
+    return num1 - num2;
+}
+function multiplicar(num1,num2){
+    return num1 * num2;
+}
+function dividir(num1,num2){
+    return num1 / num2;
+}
+function porcentaje(num1,num2){
+    return (num1 * num2) / 100;
+}
